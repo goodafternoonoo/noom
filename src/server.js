cd36fbd2f8e36c1a92aeb53ab3001c8f808e1c82
HyeonU-Jo <chw3448@gmail.com>
2023-01-26 17:59:59 +0900

    채팅/닉네임 기능 추가

--- src/server.js
+++ src/server.js
@@ -15,11 +15,27 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 wss.on("connection", (socket) => {
+    sockets.push(socket);
+    socket["nickname"] = "Anonymous";
     console.log("Connected to Browser ✅");
     socket.on("close", () => console.log("Disconnected from Browser ❌"));
-    socket.on("message", (message) => console.log(message.toString()));
-    socket.send("hello world!");
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+
+        switch (message.type) {
+            case "new_message":
+                sockets.forEach((aSocket) =>
+                    aSocket.send(`${socket.nickname}: ${message.payload}`)
+                );
+                break;
+            case "nickname":
+                socket["nickname"] = message.payload;
+                break;
+        }
+    });
 });
 
 server.listen(3000, handleListen);
